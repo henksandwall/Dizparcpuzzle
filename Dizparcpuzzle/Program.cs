3ee1c804dde5a8569f2e8ed0ecdb13c4aaf46222
henriksandwall <henrik.sandwall@cgi.com>
2021-08-26 14:11:18 +0200

    Korrekt lösning

--- Dizparcpuzzle/Program.cs
+++ Dizparcpuzzle/Program.cs
@@ -6,12 +6,12 @@ using System.Text;
 
 namespace Dizparcpuzzle
 {
+ 
     class Program
     {
-
         static void Main(string[] args)
         {
-            
+
             List<int> inputList = new List<int>();
             StreamReader inputFile = new StreamReader(@"input.txt");
             // Part 1
@@ -27,35 +27,58 @@ namespace Dizparcpuzzle
             inputFile.Close();
             System.Console.WriteLine("--------------------------Answer part 1:---------------------------");
             System.Console.WriteLine("Resulting frequency is : {0}", inputList.Sum().ToString()); //536
-            /*End Part1*/ 
+            /*End Part1*/
 
             // Part 2
-            List<int> frequencyList = inputList;
+
             HashSet<int> latestFrequencies = new HashSet<int>(); //Hash = unique
             int frequency = 0;
             int firstRepeatedFrequency = 0;
-
-            for (int i = 0; i < frequencyList.Count; i++)
+            bool duplicateFound = false;
+            //Ny version
+            while (!duplicateFound) 
             {
-                int change = frequencyList[i];
-                frequency = frequency + change;
-
-                if(latestFrequencies.Add(frequency) == false) //false if freq exists
+                foreach (var item in inputList)
                 {
-                    firstRepeatedFrequency = frequency;
-                    break;
-                        
-                }
-
-                if(i == frequencyList.Count  - 1)
-                {
-                    i -= 1;
+                    frequency += item;
+                    if (latestFrequencies.Contains(frequency))
+                    {
+                        firstRepeatedFrequency = frequency;
+                        duplicateFound = true;
+                        break;
+                    }
+                    latestFrequencies.Add(frequency);
                 }
             }
+
+            //gamla versionen fast förbättrad
+            //while (!duplicateFound)
+            //{
+            //    for (int i = 0; i < inputList.Count; i++)
+            //    {
+            //        int change = inputList[i];
+            //        frequency = frequency + change;
+
+            //        if (latestFrequencies.Add(frequency) == false) //false if freq exists
+            //        {
+            //            firstRepeatedFrequency = frequency;
+            //            duplicateFound = true;
+            //            break;
+
+            //        }
+
+            //        if (i == inputList.Count - 1)
+            //        {
+            //            i -= 1;
+            //            break;
+            //        }
+            //    }
+            //}
+
             System.Console.WriteLine("--------------------------Answer part 2:---------------------------");
-            System.Console.WriteLine("The first repeated frequency is: {0}", firstRepeatedFrequency.ToString()); //35
+            System.Console.WriteLine("The first repeated frequency is: {0}", firstRepeatedFrequency.ToString()); //75108
             System.Console.WriteLine("--------------------------The end----------------------------");
-            /*End part2*/
+            ///*End part2*/
 
 
 
@@ -63,5 +86,6 @@ namespace Dizparcpuzzle
 
         }
     }
+
 }
 
